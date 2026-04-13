2f639066be66649575267a62e07d6da94af01a81
Abdullah Sharba <absha24@student.sdu.dk>
2026-04-13 12:29:20 +0200

    IConnect.java Interface implemented and ready for common use of the modules.

---
+++ common/src/main/java/dk/sdu/st4/common/Interfaces/IConnect.java
@@ -0,0 +1,17 @@
+﻿package dk.sdu.st4.common.Interfaces;
+
+import java.util.concurrent.CompletableFuture;
+
+public interface IConnect {
+    int getMachineId();
+    void setMachineId(int machineId);
+
+    String getMachineType();
+    void setMachineType(String machineType);
+
+    void addMachine(int machineId, String machineType);
+    void removeMachine(int machineId);
+    CompletableFuture<Void> connectMachine(int machineId);
+    void disconnectMachine(int machineId);
+    boolean isConnected(int machineId);
+}
