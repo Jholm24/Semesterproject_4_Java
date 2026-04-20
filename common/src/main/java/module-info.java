9129235d4e275067aa4323215af43421ae37de4e
AndreasLC <andreaslykkecaspers@gmail.com>
2026-04-20 13:16:12 +0200

    added module-info.java

---
+++ common/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module dk.sdu.st4.common {
+    requires java.sql;
+    requires com.fasterxml.jackson.databind;
+
+    exports dk.sdu.st4.common.config;
+    exports dk.sdu.st4.common.db;
+    exports dk.sdu.st4.common.util;
+    exports dk.sdu.st4.common.Interfaces;
+}
