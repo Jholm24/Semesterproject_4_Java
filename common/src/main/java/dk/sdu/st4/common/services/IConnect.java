553f5b38ed2dc5ddd5920145322f1f257f40d801
Ltechsystem <joachimlow02@gmail.com>
2026-04-25 19:08:35 +0200

    Added comments in IConnect to clarify methods

--- common/src/main/java/dk/sdu/st4/common/services/IConnect.java
+++ common/src/main/java/dk/sdu/st4/common/services/IConnect.java
@@ -3,12 +3,16 @@ package dk.sdu.st4.common.services;
 import java.util.concurrent.CompletableFuture;
 
 public interface IConnect {
+
+    // Serial number
     String getMachineId();
     void setMachineId(String machineId);
 
+    // Machine type
     String getMachineType();
     void setMachineType(String machineType);
 
+    // Machine management
     void addMachine(String machineSerialNumber,String type, String variant, String base_url);
     void removeMachine(String machineSerialNumber);
     CompletableFuture<Void> connectMachine(String machineId);
